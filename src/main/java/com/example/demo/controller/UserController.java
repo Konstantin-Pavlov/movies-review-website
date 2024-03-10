62048ad38adf3a476de340316617b3d5c6075824
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-03-11 02:32:01 +0600

    add methods: getUserById showUserById createUser getResponseEntity

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,11 +1,12 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.UserDto;
+import com.example.demo.exception.UserNotFoundException;
 import com.example.demo.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,4 +19,32 @@ public class UserController {
     public ResponseEntity<List<UserDto>> getUsers() {
         return ResponseEntity.ok(userService.getUsers());
     }
+
+    //example: http://localhost:8089/users/user?id=2
+    @GetMapping("users/user")
+    public ResponseEntity<?> getUserById(@RequestParam(name = "id", defaultValue = "1") int id) {
+        return getResponseEntity(id);
+    }
+
+    //example: http://localhost:8089/users/2
+    @GetMapping("users/{id}")
+    public ResponseEntity<?> showUserById(@PathVariable int id) {
+        return getResponseEntity(id);
+    }
+
+    @PostMapping("users")
+    public HttpStatus createUser(UserDto user) {
+        userService.createUser(user);
+        return HttpStatus.OK;
+    }
+
+    private ResponseEntity<?> getResponseEntity(int id) {
+        try {
+            UserDto user = userService.getUserById(id);
+            return ResponseEntity.ok(user);
+        } catch (UserNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
 }
