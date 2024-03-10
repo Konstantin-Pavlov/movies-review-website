6af7b2366c0fb93046533c24299cfd7fba84509d
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-03-11 02:30:56 +0600

    add method getUserById

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -2,9 +2,11 @@ package com.example.demo.service.impl;
 
 import com.example.demo.dao.UserDao;
 import com.example.demo.dto.UserDto;
+import com.example.demo.exception.UserNotFoundException;
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -28,5 +30,23 @@ public class UserServiceImpl implements UserService {
                 )
         );
         return dtos;
+    }
+
+//    @SneakyThrows // try catch -> e.printStackTrace()
+    @Override
+    public UserDto getUserById(int id) throws UserNotFoundException {
+        User user = userDao.getUserById(id).orElseThrow(
+                () -> new UserNotFoundException("Can't find user wit ID: " + id)
+        );
+        return UserDto.builder().
+                id(user.getId())
+                .name(user.getName())
+                .password(user.getPassword())
+                .build();
+    }
+
+    @Override
+    public void createUser(UserDto user) {
+        userDao.createUser(user);
     }
 }
