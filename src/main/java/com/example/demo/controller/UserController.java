d3d0a467872497456c26a269592eafc6ca9382af
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-03-10 21:30:59 +0600

    add new class

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.UserDto;
+import com.example.demo.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("users")
+    public ResponseEntity<List<UserDto>> getUsers() {
+        return ResponseEntity.ok(userService.getUsers());
+    }
+}
