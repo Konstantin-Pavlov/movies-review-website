0e4a583730ed4a0ec7b5b9a7b7dd4143caf9c071
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-03-14 02:08:24 +0600

    remove unused imports

--- src/main/java/com/example/demo/controller/DirectorController.java
+++ src/main/java/com/example/demo/controller/DirectorController.java
@@ -1,10 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.DirectorDto;
-import com.example.demo.dto.MovieDto;
-import com.example.demo.model.Director;
 import com.example.demo.service.DirectorService;
-import com.example.demo.service.MovieService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,8 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("directors")
