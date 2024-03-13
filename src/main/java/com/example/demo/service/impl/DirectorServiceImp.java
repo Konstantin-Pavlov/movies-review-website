14657520b992a934bfb8072eed468e606049fe22
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-03-14 02:04:59 +0600

    now exception is handled without SneakyThrows annotation; added logging

--- src/main/java/com/example/demo/service/impl/DirectorServiceImp.java
+++ src/main/java/com/example/demo/service/impl/DirectorServiceImp.java
@@ -5,21 +5,28 @@ import com.example.demo.dto.DirectorDto;
 import com.example.demo.model.Director;
 import com.example.demo.service.DirectorService;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class DirectorServiceImp implements DirectorService {
     private final DirectorDao directorDao;
-    @SneakyThrows
+
+    //    @SneakyThrows
     @Override
     public DirectorDto getDirectorById(long id) {
-        Director director = directorDao.getDirectorById(id)
-                .orElseThrow(() -> new Exception("Can't find director with id " + id));
-        return DirectorDto.builder()
-                .id(director.getId())
-                .fullName(director.getFullName())
-                .build();
+        try {
+            Director director = directorDao.getDirectorById(id)
+                    .orElseThrow(() -> new Exception("Can't find director with id " + id));
+            return DirectorDto.builder()
+                    .id(director.getId())
+                    .fullName(director.getFullName())
+                    .build();
+        } catch (Exception e) {
+            log.error("Can't find director with id " + id);
+        }
+        return null;
     }
 }
