208ff2f66acdaa5b98fe02e8797fc595e448badf
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-03-18 16:40:54 +0600

    add JsonProperty to releaseYear field and change package name from demo to movie_review

--- src/main/java/com/example/movie_review/dto/MovieDto.java
+++ src/main/java/com/example/movie_review/dto/MovieDto.java
@@ -1,5 +1,6 @@
-package com.example.demo.dto;
+package com.example.movie_review.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 public class MovieDto {
     private long id;
     private String name;
+    @JsonProperty("release_year") // wired to database
     private Integer releaseYear;
     private String description;
     private DirectorDto director;
