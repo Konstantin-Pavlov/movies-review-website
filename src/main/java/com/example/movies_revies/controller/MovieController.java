56efb5e49bb197b35b9e641d8a18298588fb6602
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-03-18 16:22:21 +0600

    add movie controller

---
+++ src/main/java/com/example/movies_revies/controller/MovieController.java
@@ -0,0 +1,43 @@
+package com.example.movies_revies.controller;
+
+import com.example.movies_revies.dto.MovieDto;
+import com.example.movies_revies.exception.MovieNotFoundException;
+import com.example.movies_revies.service.MovieService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("movies")
+@RequiredArgsConstructor
+public class MovieController {
+    private final MovieService movieService;
+
+    @GetMapping
+    public ResponseEntity<List<MovieDto>> getMovies() {
+        return ResponseEntity.ok(movieService.getMovies());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<?> getMovieById(@PathVariable long id) {
+        try {
+            return ResponseEntity.ok(movieService.getMovieById(id));
+        } catch (MovieNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @PostMapping("add-and-return-id")
+    public ResponseEntity<?> createAndReturnId(@RequestBody MovieDto movieDto) {
+        return ResponseEntity.ok(movieService.createMovieAndReturnId(movieDto));
+    }
+
+    @PostMapping("add")
+    public HttpStatus create(@RequestBody MovieDto movieDto) {
+        movieService.createMovie(movieDto);
+        return HttpStatus.OK;
+    }
+}
