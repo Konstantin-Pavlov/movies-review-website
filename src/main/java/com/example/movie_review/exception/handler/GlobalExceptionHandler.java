d66552d4f0e1ac7affb7102811a1878e9889c896
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-03-25 15:07:52 +0600

    changed methods

--- src/main/java/com/example/movie_review/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/movie_review/exception/handler/GlobalExceptionHandler.java
@@ -1,6 +1,11 @@
 package com.example.movie_review.exception.handler;
 
+import com.example.movie_review.exception.ErrorResponseBody;
+import com.example.movie_review.exception.MovieNotFoundException;
+import com.example.movie_review.service.ErrorService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,15 +13,21 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.NoSuchElementException;
 
+@RequiredArgsConstructor
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(NoSuchElementException.class)
-    public ErrorResponse noSuchElement(NoSuchElementException exception) {
-        return ErrorResponse.builder(exception, HttpStatus.NOT_FOUND, exception.getMessage()).build();
+    private final ErrorService errorService;
+
+    //    @ExceptionHandler(NoSuchElementException.class)
+    @ExceptionHandler(MovieNotFoundException.class)
+    public ResponseEntity<ErrorResponseBody>/* ErrorResponse*/ noSuchElement(MovieNotFoundException exception) {
+//        return ErrorResponse.builder(exception, HttpStatus.NOT_FOUND, exception.getMessage()).build();
+        return new ResponseEntity<>(errorService.makeResponse(exception), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ErrorResponse validationHandler(MethodArgumentNotValidException exception){
-        return ErrorResponse.builder(exception, HttpStatus.BAD_REQUEST, exception.getMessage()).build();
+    public /*ErrorRespons*/ ResponseEntity<ErrorResponseBody> validationHandler(MethodArgumentNotValidException exception) {
+//        return ErrorResponse.builder(exception, HttpStatus.BAD_REQUEST, exception.getMessage()).build();
+        return new ResponseEntity<>(errorService.makeResponse(exception.getBindingResult()), HttpStatus.BAD_REQUEST);
     }
 }
